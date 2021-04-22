f20dc39fdc60a1a97e16d0790983d057bef56a89
Joel Boersma <joelboersma@gmail.com>
2021-04-22 16:45:06 -0700

    errors, focus, screen-switching

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -5,44 +5,75 @@
 
    const myForm = document.querySelector('#MyForm');
    const madLib = document.querySelector('#MadLib');
+   const formError = document.querySelector('#formError');
+   const formSection = document.querySelector('#formSection');
+   const formButton = document.querySelector('input[type="submit"]');
    const formData = document.querySelectorAll('section input');
+   
+
+   let madLibShowing = false;
 
    myForm.addEventListener('submit', function(event) {
       event.preventDefault();
-      processFormData();
+      if (madLibShowing) {
+         showForm();
+         // madLibShowing = false;
+      }
+      else {
+         processFormData();
+      }
    });
 
    function processFormData() {
-      let emptyFields = 0;
       const words = [];
 
       // get form values and clear inputs
       for (const field of formData) {
          if (field.value) {
             words.push(field.value);
-            field.value = '';
          }
          else {
-            emptyFields++;
+            break;
          }
       }
 
-      if (emptyFields > 0) {
-         madLib.innerHTML = 'Please fill out every field.'
+      // if not every field is filled out
+      if (words.length < formData.length) {
+         formError.innerHTML = 'Error: Please fill out every field.'
+         formError.removeAttribute('hidden');
       }
       else {
-         makeMadLib(words);
+         for (const field of formData) {
+            field.value = '';
+         }
+         formError.setAttribute('hidden', 'hidden');
+         showMadLib(words);
       }
       
       words.length = 0;
    }
 
-   function makeMadLib(words) {
+   function showMadLib(words) {
+      // Example code for showing results
       let result = 'This is an example for the thing:'
       for(const word of words) {
          result += ` ${word}`;
       }
+
+      formSection.setAttribute('hidden', 'hidden');
+      madLib.removeAttribute('hidden')
+      formButton.setAttribute('value', 'Create Another Adventure');
+      formButton.classList = 'restartButton';
       madLib.innerHTML = result
+      madLibShowing = true;
+   }
+
+   function showForm() {
+      formSection.removeAttribute('hidden');
+      madLib.setAttribute('hidden', 'hidden');
+      formButton.setAttribute('value', 'Blast Off');
+      formButton.classList = '';
+      madLibShowing = false;
    }
 
 
