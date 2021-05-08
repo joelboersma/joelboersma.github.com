e6fe50df06bd3dc2c171cf8df043abb33619afb6
Joel Boersma <joelboersma@gmail.com>
2021-05-08 16:50:49 -0700

    mousewheel detection'

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -1,5 +1,34 @@
 (function(){
    'use strict';
 
-   
-}())
+   let scrollingNow = false;
+
+   // Listen for mouse wheel
+   window.addEventListener('wheel', function(event) {
+
+      if (!scrollingNow) {
+         scrollingNow = true;
+
+         // Which direction is the scroll?
+         if (event.deltaY > 0) {
+            // scrolling down
+            console.log('scrolling down');
+         }
+         else if (event.deltaY < 0) {
+            // scrolling up
+            console.log('scrolling up');
+         }
+         else {
+            // scrolling 0 (shouldn't happen)
+            console.log('scrolling 0');
+         }
+
+         setTimeout(function() {
+            scrollingNow = false;
+            console.log('done!');
+         }, 2000);
+      }
+
+   });
+
+}());
