1b15656a2d104c2ca594d87a3309cb38edff7667
Joel Boersma <joelboersma@gmail.com>
2021-05-10 12:30:39 -0700

    scrolling slideshow for images works

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -2,6 +2,7 @@
    'use strict';
    // console.log('reading js');
 
+   const images = document.querySelectorAll('#pictures img');
    const numImages = 6;
    let focusImg = 0;
    let scrollingNow = false;
@@ -45,6 +46,25 @@
 
    function transitionImage() {
       console.log(focusImg);
+
+      for (let i = 0; i < numImages; i++) {
+         const thisImg = images[i];
+         thisImg.className = '';
+
+         if (i < focusImg) {
+            thisImg.classList.add('above');
+         }
+         else if (i > focusImg) {
+            thisImg.classList.add('below');
+         }
+         else {
+            thisImg.classList.add('focus');
+         }
+
+         if (i > focusImg + 1 || i < focusImg - 1) {
+            thisImg.classList.add('hidden');
+         }
+      }
    }
 
 }());
