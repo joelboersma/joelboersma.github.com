0bd9de8f532900098b7708b9d64fd972b29e66b3
Joel Boersma <44932998+joelboersma@users.noreply.github.com>
2021-05-10 17:32:04 -0700

    descriptions.js init

---
+++ des157/studio2/descriptions.js
@@ -0,0 +1,26 @@
+const descriptions = [
+   {
+      "date": "July 4, 2018",
+      "description": "Tiger under da bed"
+   },
+   {
+      "date": "May 19, 2020",
+      "description": ""
+   },
+   {
+      "date": "June 22, 2020",
+      "description": "Sally in a suitcase"
+   },
+   {
+      "date": "",
+      "description": ""
+   },
+   {
+      "date": "",
+      "description": ""
+   },
+   {
+      "date": "",
+      "description": ""
+   }
+];
